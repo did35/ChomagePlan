608e88635f5745321475a8e9e7aa43f7523bbced
Didier Delhaisse <did35@yahoo.com>
2025-06-08 09:22:27 +0200

    Initial Commit

---
+++ ChomagePlan/ChomagePlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChomagePlanApp.swift
+//  ChomagePlan
+//
+//  Created by Didier Delhaisse on 08/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChomagePlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
